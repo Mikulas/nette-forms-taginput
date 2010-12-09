73156398ec90d2b0c719aa93bf41d7dfdc860b91
Mikuláš Dítě <mikulas@dite.cz>
2010-12-09 20:45:23 +0100

    Added serverside validations

--- TagInput.php
+++ TagInput.php
@@ -64,9 +64,15 @@ class TagInput extends TextInput
 
 
 
-	public function  addRule($operation, $message = NULL, $arg = NULL)
+	public function addRule($operation, $message = NULL, $arg = NULL)
 	{
-		throw new \NotImplementedException('');
+		//throw new \NotImplementedException('');
+		switch($operation) {
+			case Form::EQUAL:
+				if (!is_array($arg))
+					throw new \InvalidArgumentException(__METHOD__ . '(' . $operation . ') must be compared to array.');
+		}
+
 		parent::addRule($operation, $message, $arg);
 	}
 
@@ -103,5 +109,189 @@ class TagInput extends TextInput
 	public static function addTagInput(Form $form, $name, $label = NULL)
 	{
 		return $form[$name] = new self($label);
+	}
+
+
+
+	/**
+	 * VALIDATORS
+	 */
+
+	/**
+	 * Equal validator: are control's value and second parameter equal?
+	 * @param  IFormControl
+	 * @param  mixed
+	 * @return bool
+	 */
+	public static function validateEqual(IFormControl $control, $arg)
+	{
+		$value = $control->getValue();
+		foreach ((is_array($value) ? $value : array($value)) as $val) {
+			foreach ((is_array($arg) ? $arg : array($arg)) as $item) {
+				if ((string) $val === (string) ($item instanceof IFormControl ? $item->value : $item)) {
+					return TRUE;
+				}
+			}
+		}
+		return FALSE;
+	}
+
+
+
+	/**
+	 * Filled validator: is control filled?
+	 * @param  IFormControl
+	 * @return bool
+	 */
+	public static function validateFilled(IFormControl $control)
+	{
+		return count($control->getValue()) !== 0;
+	}
+
+
+
+	/**
+	 * Min-length validator: has control's value minimal length?
+	 * @param  TextBase
+	 * @param  int  length
+	 * @return bool
+	 */
+	public static function validateMinLength(TextBase $control, $length)
+	{
+		return count($control->getValue()) >= $length;
+	}
+
+
+
+	/**
+	 * Max-length validator: is control's value length in limit?
+	 * @param  TextBase
+	 * @param  int  length
+	 * @return bool
+	 */
+	public static function validateMaxLength(TextBase $control, $length)
+	{
+		return count($control->getValue()) <= $length;
+	}
+
+
+
+	/**
+	 * Length validator: is control's value length in range?
+	 * @param  TextBase
+	 * @param  array  min and max length pair
+	 * @return bool
+	 */
+	public static function validateLength(TextBase $control, $range)
+	{
+		if (!is_array($range)) {
+			$range = array($range, $range);
+		}
+		$len = count($control->getValue());
+		return ($range[0] === NULL || $len >= $range[0]) && ($range[1] === NULL || $len <= $range[1]);
+	}
+
+
+
+	/**
+	 * Email validator: is control's value valid email address?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateEmail(TextBase $control)
+	{
+		throw new \LogicException(':EMAIL validator is not applicable to TagControl.');
+	}
+
+
+
+	/**
+	 * URL validator: is control's value valid URL?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateUrl(TextBase $control)
+	{
+		throw new \LogicException(':URL validator is not applicable to TagControl.');
+	}
+
+
+
+	/** @deprecated */
+	public static function validateRegexp(TextBase $control, $regexp)
+	{
+		throw new \LogicException(':REGEXP validator is not applicable to TagControl.');
+	}
+
+
+
+	/**
+	 * Regular expression validator: matches control's value regular expression?
+	 * @param  TextBase
+	 * @param  string
+	 * @return bool
+	 */
+	public static function validatePattern(TextBase $control, $pattern)
+	{
+		throw new \LogicException(':PATTERN validator is not applicable to TagControl.');
+	}
+
+
+
+	/**
+	 * Integer validator: is each value of tag of control decimal number?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateInteger(TextBase $control)
+	{
+		foreach ($control->getValue() as $tag) {
+			if (!String::match($tag, '/^-?[0-9]+$/')) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * Float validator: is each value of tag of control value float number?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateFloat(TextBase $control)
+	{
+		foreach ($control->getValue() as $tag) {
+			if (!String::match($tag, '/^-?[0-9]*[.,]?[0-9]+$/')) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * Range validator: is a control's value number in specified range?
+	 * @param  TextBase
+	 * @param  array  min and max value pair
+	 * @return bool
+	 */
+	public static function validateRange(TextBase $control, $range)
+	{
+		throw new \LogicException(':RANGE validator is not applicable to TagControl.');
+	}
+
+
+
+	/**
+	 * Uniqueness validator: is each value of tag of control unique?
+	 * @param  TagInput
+	 * @return bool
+	 */
+	public static function validateUnique(TagInput $control)
+	{
+		return count(array_unique($control->getValue())) === count($control->getValue());
 	}
 }
