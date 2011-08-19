07a1e077ccb57bc23aa2ed5d95074a2de5e9d4a9
Mikuláš Dítě <rullaf@gmail.com>
2011-08-19 10:14:26 +0200

    2.0 init

---
+++ TagInput.php
@@ -0,0 +1,434 @@
+<?php
+
+/**
+ * Tag Input for html forms
+ * @author Mikulas Dite
+ * @copyright Mikulas Dite 2010-11
+ */
+
+namespace Nette\Forms\Controls;
+
+use Nette\Forms\Form;
+use Nette\Forms\IControl;
+use Nette\Utils\Strings;
+use Nette\Application\Responses\JsonResponse;
+
+
+class TagInput extends TextInput
+{
+
+	/** @var string rule */
+	const UNIQUE = ':unique';
+
+
+
+	/** @var string */
+	private $renderName;
+
+
+
+	/** @var int */
+	protected $payloadLimit = 5;
+
+	/** @var string regex */
+	protected $delimiter = '[\s,]+';
+
+	/** @var string */
+	protected $joiner = ', ';
+
+	/** @var callback returning array */
+	protected $suggestCallback;
+
+
+
+	/**
+	 * @param string $delimiter regex
+	 * @return TagInput provides fluent interface
+	 */
+	public function setDelimiter($delimiter)
+	{
+		$this->delimiter = $delimiter;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string $delimiter
+	 * @return TagInput provides fluent interface
+	 */
+	public function setJoiner($joiner)
+	{
+		$this->joiner = $joiner;
+		return $this;
+	}
+
+
+
+	/**
+	 * @return array
+	 */
+	public function getValue()
+	{
+		// temporarily disable filters
+		$filters = $this->filters;
+		$this->filters = array();
+
+		$res = Strings::split(parent::getValue(), "\x01" . $this->delimiter . "\x01");
+		$this->filters = $filters;
+
+		foreach ($res as &$tag) {
+			foreach ($this->filters as $filter) {
+				$tag = $filter($tag);
+			}
+			if (!$tag) {
+				unset($tag);
+			}
+		}
+
+		return $res;
+	}
+
+
+
+	/**
+	 * Generates control's HTML element.
+	 * @return \Nette\Utils\Html
+	 */
+	public function getControl()
+	{
+		$container = \Nette\Utils\Html::el('div');
+		/** @var $control \Nette\Utils\Html */
+		$control = parent::getControl();
+
+		if ($this->delimiter !== NULL && Strings::trim($this->delimiter) !== '') {
+			$control->attrs['data-tag-delimiter'] = $this->delimiter;
+		}
+
+		if ($this->joiner !== NULL && Strings::trim($this->joiner) !== '') {
+			$control->attrs['data-tag-joiner'] = $this->joiner;
+		}
+	
+		if ($this->suggestCallback !== NULL) {
+			$form = $this->getForm();
+			if (!$form || !$form instanceof Form) {
+				throw new InvalidStateException("TagInput supports only Nette\\Application\\UI\\Form.");
+			}
+			$control->attrs['data-tag-suggest'] = $form->getPresenter()->link($this->renderName, array('word_filter' => '%__filter%'));
+		}
+		$container->add($control);
+		$container->add(\Nette\Utils\Html::el('script')->setHtml('TagInput.create("#frm' . $this->form->name . '-' . $this->name . '");'));
+
+		return $container;
+	}
+
+
+
+	/**
+	 * Sets control's value.
+	 * @param  string
+	 * @return TextBase  provides a fluent interface
+	 */
+	public function setValue($value)
+	{
+		if (!is_array($value)) {
+			$value = explode(trim($this->joiner), $value);
+		}
+		parent::setValue(implode($this->joiner, $value));
+		return $this;
+	}
+
+
+
+	/**
+	 * @param array $value
+	 * @return TagInput provides fluent interface
+	 */
+	public function setDefaultValue($value)
+	{
+		if (!is_array($value)) {
+			throw new \Nette\InvalidArgumentException("Invalid argument type passed to " . __METHOD__ . ", expected array.");
+		}
+		parent::setDefaultValue(implode($this->joiner, $value));
+		return $this;
+	}
+
+
+
+	/**
+	 * @param int $limit
+	 * @return TagInput provides fluent interface
+	 */
+	public function setPayloadLimit($limit)
+	{
+		if ($limit < 0)
+			throw new \Nette\InvalidArgumentException("Invalid limit, expected positive integer.");
+
+		$this->payloadLimit = $limit;
+		return $this;
+	}
+
+
+	/**
+	 * Adds a validation rule.
+	 * @param  mixed      rule type
+	 * @param  string     message to display for invalid data
+	 * @param  mixed      optional rule arguments
+	 * @return FormControl  provides a fluent interface
+	 */
+	public function addRule($operation, $message = NULL, $arg = NULL)
+	{
+		switch($operation) {
+			case Form::EQUAL:
+				if (!is_array($arg))
+					throw new \Nette\InvalidArgumentException(__METHOD__ . '(' . $operation . ') must be compared to array.');
+		}
+
+		return parent::addRule($operation, $message, $arg);
+	}
+
+
+
+	/**
+	 * @param array $suggest
+	 * @return TagInput provides fluent interface
+	 */
+	public function setSuggestCallback($suggest)
+	{
+		$this->suggestCallback = callback($suggest);
+		return $this;
+	}
+
+
+
+	/**
+	 * @param \Nette\Application\UI\Presenter presenter
+	 * @param string presenter
+	 */
+	public function renderResponse($presenter, $filter)
+	{
+		$data = array();
+		if (!($this->suggestCallback instanceof \Nette\Callback)) {
+			throw new \Nette\InvalidStateException('Callback not set.');
+		}
+
+		foreach ($this->suggestCallback->invoke($filter, $this->payloadLimit) as $tag) {
+			if (count($data) >= $this->payloadLimit) {
+				break;
+			}
+			$data[] = (string) $tag;
+		}
+
+		$presenter->sendResponse(new JsonResponse($data));
+	}
+
+
+
+	/********************* registration *******************/
+
+
+
+	/**
+	 * Adds addTag() method to \Nette\Forms\Form
+	 */
+	public static function register()
+	{
+		Form::extensionMethod('addTag', callback(__CLASS__, 'addTag'));
+	}
+
+
+
+	/**
+	 * @param Form $form
+	 * @param string $name
+	 * @param string $label
+	 * @param array $suggest
+	 * @return TagInput provides fluent interface
+	 */
+	public static function addTag(Form $form, $name, $label = NULL)
+	{
+		$form[$name] = new self($label);
+		$form[$name]->renderName = 'tagInputSuggest' . ucfirst($name);
+		return $form[$name];
+	}
+
+
+
+	/********************* validation *********************/
+
+
+
+	/**
+	 * Equal validator: are control's value and second parameter equal?
+	 * @param  IControl
+	 * @param  mixed
+	 * @return bool
+	 */
+	public static function validateEqual(IControl $control, $arg)
+	{
+		$value = $control->getValue();
+		sort($value);
+		sort($arg);
+		return $value === $arg;
+	}
+
+
+
+	/**
+	 * Filled validator: is control filled?
+	 * @param  IControl
+	 * @return bool
+	 */
+	public static function validateFilled(IControl $control)
+	{
+		return count($control->getValue()) !== 0;
+	}
+
+
+
+	/**
+	 * Min-length validator: has control's value minimal length?
+	 * @param  TextBase
+	 * @param  int  length
+	 * @return bool
+	 */
+	public static function validateMinLength(TextBase $control, $length)
+	{
+		return count($control->getValue()) >= $length;
+	}
+
+
+
+	/**
+	 * Max-length validator: is control's value length in limit?
+	 * @param  TextBase
+	 * @param  int  length
+	 * @return bool
+	 */
+	public static function validateMaxLength(TextBase $control, $length)
+	{
+		return count($control->getValue()) <= $length;
+	}
+
+
+
+	/**
+	 * Length validator: is control's value length in range?
+	 * @param  TextBase
+	 * @param  array  min and max length pair
+	 * @return bool
+	 */
+	public static function validateLength(TextBase $control, $range)
+	{
+		if (!is_array($range)) {
+			$range = array($range, $range);
+		}
+		$len = count($control->getValue());
+		return ($range[0] === NULL || $len >= $range[0]) && ($range[1] === NULL || $len <= $range[1]);
+	}
+
+
+
+	/**
+	 * Email validator: is control's value valid email address?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateEmail(TextBase $control)
+	{
+		throw new \LogicException(':EMAIL validator is not applicable to TagInput.');
+	}
+
+
+
+	/**
+	 * URL validator: is control's value valid URL?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateUrl(TextBase $control)
+	{
+		throw new \LogicException(':URL validator is not applicable to TagInput.');
+	}
+
+
+
+	/** @deprecated */
+	public static function validateRegexp(TextBase $control, $regexp)
+	{
+		throw new \LogicException(':REGEXP validator is not applicable to TagInput.');
+	}
+
+
+
+	/**
+	 * Regular expression validator: matches control's value regular expression?
+	 * @param  TextBase
+	 * @param  string
+	 * @return bool
+	 */
+	public static function validatePattern(TextBase $control, $pattern)
+	{
+		throw new \LogicException(':PATTERN validator is not applicable to TagInput.');
+	}
+
+
+
+	/**
+	 * Integer validator: is each value of tag of control decimal number?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateInteger(TextBase $control)
+	{
+		foreach ($control->getValue() as $tag) {
+			if (!Strings::match($tag, '/^-?[0-9]+$/')) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * Float validator: is each value of tag of control value float number?
+	 * @param  TextBase
+	 * @return bool
+	 */
+	public static function validateFloat(TextBase $control)
+	{
+		foreach ($control->getValue() as $tag) {
+			if (!Strings::match($tag, '/^-?[0-9]*[.,]?[0-9]+$/')) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+
+
+	/**
+	 * Range validator: is a control's value number in specified range?
+	 * @param  TextBase
+	 * @param  array  min and max value pair
+	 * @return bool
+	 */
+	public static function validateRange(TextBase $control, $range)
+	{
+		throw new \LogicException(':RANGE validator is not applicable to TagInput.');
+	}
+
+
+
+	/**
+	 * Uniqueness validator: is each value of tag of control unique?
+	 * @param  TagInput
+	 * @return bool
+	 */
+	public static function validateUnique(TagInput $control)
+	{
+		return count(array_unique($control->getValue())) === count($control->getValue());
+	}
+
+}
---
+++ tagInput.js
@@ -0,0 +1,365 @@
+/**
+ * Tag Input for html forms
+ * @author Mikuláš Dítě
+ * @copyright Mikuláš Dítě 2010-11
+ */
+
+function d() {console.log(arguments);}
+
+function Keyboard() {
+	this.remove = function(e) {
+		return e.keyCode == 8;
+	};
+	this.tab = function(e) {
+		return e.keyCode == 9;
+	};
+	this.enter = function(e) {
+		return e.keyCode == 13;
+	};
+	this.escape = function(e) {
+		return e.keyCode == 27;
+	};
+	this.left = function(e) {
+		return e.keyCode == 37;
+	};
+	this.up = function(e) {
+		return e.keyCode == 38;
+	};
+	this.right = function(e) {
+		return e.keyCode == 39;
+	};
+	this.down = function(e) {
+		return e.keyCode == 40;
+	};
+	this.del = function(e) {
+		return e.keyCode == 46;
+	};
+	this.control = function(e) {
+		return $.inArray(e.keyCode, [8, 16, 17, 18, 35, 36, 37, 38, 39, 40, 46, 91, 92]) !== -1;
+	};
+	this.whitespace = function(e) {
+		return $.inArray(e.keyCode, [13, 32]) !== -1;
+	};
+	this.input = function(e) {
+		//d(e.keyCode, this.keyToChar(e.keyCode));
+		if ($.inArray(e.keyCode, [96, 189]) !== -1) // numpad zero, hyphen
+			return true;
+		var letter = /^[a-iA-Z\u00C0-\u02A00-9-]+$/; // a-i numpad 1-9, A-Z letters, accented letters, numbers, hyphen
+		return letter.test(this.keyToChar(e.keyCode));
+	};
+
+	this.keyToChar = function(key) {
+		return String.fromCharCode(key);
+	};
+}
+
+var kb = new Keyboard;
+
+function TagInputControl() {
+	var i = this;
+
+	this.name = undefined;
+	this.container = undefined;
+	this.control = undefined;
+	this.tags = undefined;
+	this.helper = undefined;
+
+	this.rules = undefined;
+	
+	this.onKeyDown = function(e) {
+		if (kb.input(e)) { // just writing letters
+			i.tags.children('.focus').removeClass('focus');
+			i.stretchHelperToContainer();
+
+		} else if (kb.del(e) && i.tags.children('.focus').length) {
+			var selected = i.tags.children('.focus');
+			if (selected.next().length)
+				selected.next().addClass('focus');
+			else if (selected.prev().length)
+				selected.prev().addClass('focus');
+			selected.remove();
+
+		} else if (kb.remove(e) && i.helper.getCaret() === 0) { // remove last tag
+			i.tags.children(':last-child').remove();
+
+		} else if ((kb.left(e) || kb.right(e)) && i.helper.val() === '') { // tag selection
+			var selected = i.tags.children('.focus');
+			if (kb.left(e) && !selected.length) { // select last tag
+				selected.removeClass('focus');
+				i.tags.children(':last-child').addClass('focus');
+			} else if (kb.left(e) && selected.prev().length) {
+				selected.removeClass('focus');
+				selected.prev().addClass('focus');
+			} else if (kb.right(e)) {
+				selected.removeClass('focus');
+				selected.next().addClass('focus');
+			}
+
+		} else if (kb.escape(e)) { // throw away current input
+			i.helper.val('');
+			i.stretchHelperToContainer();
+
+		} else if (!kb.control(e)) { // create tag from helper
+			return i.helperToTag();
+		}
+	};
+
+	this.helperToTag = function() {
+		var value = i.helper.val().trim();
+		i.helper.val('');
+		i.createTag(value);
+		i.stretchHelperToContainer();
+		return false;
+	};
+
+	this.onChange = function() {
+		return false;
+	};
+
+	this.focus = function() {
+		i.helper.focus();
+	};
+
+	this.onFocus = function() {
+		i.container.addClass('focus');
+	};
+
+	this.onBlur = function() {
+		i.container.removeClass('focus');
+		i.tags.children().removeClass('focus');
+		i.helperToTag();
+	};
+
+	this.isUnique = function() {
+		$.each(i.rules, function(index, rule) {
+			if (rule.op === ':unique') return true;
+		});
+		return false;
+	};
+
+	this.getMaxLen = function() {
+		$.each(i.rules, function(index, rule) {
+			if ((rule.op === ':maxLength' || rule.op === ':length') && rule.arg > length)
+				return rule.arg;
+		});
+		return undefined;
+	};
+
+	this.getDelimiter = function() {
+		return i.control.data('tag-delimiter');
+	};
+
+	this.val = function(values) {
+		if (arguments.length === 0) {
+			var value = [];
+			i.tags.find('.tag').each(function() {
+				value.push($(this)[0].firstChild.textContent);
+			});
+			return value;
+		}
+		i.tags.children().remove();
+		$.each(values, function(x, v) {
+			i.createTag(v);
+		});
+	};
+
+	this.createTag = function(value) {
+		value = value.trim();
+		if (!value)
+			return false;
+
+		var tag = $('<span/>').addClass('tag');
+		tag.text(value);
+		tag.click(function() {
+			i.focus();
+			$(this).addClass('focus');
+		});
+		var del = $('<span/>').addClass('tag-delete').html('&times;');
+		tag.append(del);
+		del.click(function() {
+			tag.remove();
+			i.stretchHelperToContainer();
+			i.focus();
+		});
+		i.tags.append(tag);
+		i.val();
+	};
+
+	this.stretchHelperToContainer = function() {
+		i.helper.css('width', 0);
+		var width = i.container.position()['left'] + i.container.width() - i.helper.position()['left'];
+		width = width < 50 ? i.container.width() : width;
+		i.helper.css('width', width);
+	};
+
+	this.getForm = function() {
+		return i.control.parents('form');
+	};
+
+	this.save = function() {
+		i.control.val(i.val().join(','));
+		return false;
+	};
+}
+
+var TagInput = TagInput || {inputs: []};
+
+TagInput.create = function(control) {
+	var input = new TagInputControl;
+
+	input.name = control;
+	input.container = $('<div/>').addClass('tag-container');
+	input.control = $(control);
+	input.tags = $('<span/>');
+	input.helper = $('<input type="text"/>');
+
+	input.control.hide();
+
+	input.control.after(input.container);
+	input.container.append(input.control);
+	input.container.append(input.tags);
+	input.container.append(input.helper);
+
+	input.helper.keydown(input.onKeyDown);
+	input.helper.bind('paste cut change', input.onChange);
+
+	input.val(input.control.val().split(','));
+
+	input.stretchHelperToContainer();
+
+	input.getForm().submit(function() {
+		input.save();
+	});
+
+	input.helper.focus(input.onFocus);
+	input.helper.blur(input.onBlur);
+	input.control.focus = input.container.focus = input.focus;
+
+	input.rules = eval('[' + (input.control.data('nette-rules') || '') + ']');
+
+	this.inputs.push(input);
+};
+
+/********************* caret position *****************/
+
+$.fn.getCaret = function(pos) {
+	if ($(this).get(0).createTextRange) {
+		var r = document.selection.createRange().duplicate();
+		r.moveEnd('character', pos);
+		if (r.text == '')
+			return $(this).get(0).value.length;
+		return $(this).get(0).value.lastIndexOf(r.text);
+	} else {
+		return $(this).get(0).selectionStart;
+	}
+};
+
+/********************* validation *********************/
+
+
+$.fn.validateTagInput = function(onlyCheck) {
+	$control = $(this);
+	var tags = $(this).getTagInputValues();
+	var rules = eval('[' + ($control.children('.tag-control').data('nette-rules') || '') + ']');
+
+	success = true;
+	$.each(rules, function (id, rule) {
+		var op = rule.op.match(/(~)?([^?]+)/);
+		rule.neg = op[1];
+		rule.op = op[2];
+		if (!Nette.validateRuleTagControl(tags, rule.op, rule.arg)) {
+			if (typeof onlyCheck == 'undefined' || !onlyCheck) {
+				Nette.addError($control.get(0), rule.msg.replace('%value', $control.getTagInputValues().join(', ')));
+			}
+			success = false;
+		}
+	});
+
+	return success;
+};
+
+
+jQuery.fn.compare = function(t) {
+    if (this.length != t.length) {return false;}
+    var a = this.sort(),
+        b = t.sort();
+    for (var i = 0; t[i]; i++) {
+        if (a[i] !== b[i]) {
+                return false;
+        }
+    }
+    return true;
+};
+
+
+Nette.validateRuleTagControl = function(tags, op, arg)
+{
+	switch (op) {
+	case ':filled':
+		return tags.length !== 0;
+
+	case ':equal':
+		arg = arg instanceof Array ? arg : [arg];
+		return $(tags).compare(arg);
+
+	case ':minLength':
+		return tags.length >= arg;
+
+	case ':maxLength':
+		return tags.length <= arg;
+
+	case ':length':
+		if (typeof arg !== 'object') {
+			arg = [arg, arg];
+		}
+		return (arg[0] === null || tags.length >= arg[0]) && (arg[1] === null || tags.length <= arg[1]);
+
+	case ':integer':
+		success = true;
+		$.each(tags, function(index, tag) {
+			if (!tag.match(/^-?[0-9]+$/))
+				success = false;
+		});
+		return success;
+
+	case ':float':
+		success = true;
+		$.each(tags, function(index, tag) {
+			if (!tag.match(/^-?[0-9]*[.,]?[0-9]+$/))
+				success = false
+		});
+		return success;
+
+	case ':unique':
+		return $.unique(tags) === tags;
+	}
+
+	if (onlyCheck === undefined || !onlyCheck) {
+		Nette.addError($control.children('.tag-control').get(0), rule.msg.replace('%value', $control.getTagInputValues().join(', ')));
+	}
+	return false;
+}
+
+
+Nette.validateForm = function(sender) {
+	var form = sender.form || sender;
+	if (form['nette-submittedBy'] && form.elements[form['nette-submittedBy']] && form.elements[form['nette-submittedBy']].getAttribute('formnovalidate') !== null) {
+		return true;
+	}
+	for (var i = 0; i < form.elements.length; i++) {
+		var elem = form.elements[i];
+		if (!(elem.nodeName.toLowerCase() in {input:1, select:1, textarea:1}) || (elem.type in {hidden:1, submit:1, image:1, reset: 1}) || elem.disabled || elem.readonly) {
+			continue;
+		}
+		if ($(elem).is('.tag-control')) {
+			if (!$(elem).parent().validateTagInput()) {
+				return false;
+			}
+			continue;
+		}
+		if (!Nette.validateControl(elem)) {
+			return false;
+		}
+	}
+	return true;
+};
